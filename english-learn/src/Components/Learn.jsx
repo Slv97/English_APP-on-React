98b75fad563a4d4d0979680aec0f6a1535ffbedf
Slv97 <65807377+Slv97@users.noreply.github.com>
2020-08-13 17:40:42 +0300

    Add component Learn

--- english-learn/src/Components/Learn.jsx
+++ english-learn/src/Components/Learn.jsx
@@ -2,9 +2,23 @@ import React from 'react';
 
 const Learn = () => {
     return (
-        <div className='learn-container'>
-            <div className='percentage'>50%</div>
-        </div>
+        <div className='learn-wrapper'>
+            <div className='learn-container'>
+                <div className='percentage'>50%</div>
+
+                <div className='word-translation'>
+                    Forest
+                </div>
+
+                <div className='word-container'>
+                    <span className='description-label'>Translation</span>
+
+                    <span className='word'>Лес</span>
+                </div>
+
+                <div className='btn-next'> &#8594;	</div>
+            </div>
+        </div>        
     )
 }
 
